c7b974d4fe64322b0282fefe534e7b33aa19c47f
michel <1024836+michel-lacle@users.noreply.github.com>
2020-12-16 14:50:17 -0500

    working compile script

--- compile.js
+++ compile.js
@@ -4,12 +4,6 @@ const solc = require('solc');
 
 const dirname = path.resolve();
 const inboxPath = path.resolve(dirname, 'contracts', 'Inbox.sol');
-console.log(inboxPath);
 const source = fs.readFileSync(inboxPath, 'utf8');
 
-console.log(source);
-
-const bytecode = solc.compile(source, 1);
-
-console.log(bytecode);
-
+module.exports = solc.compile(source, 1).contracts[':Inbox']
