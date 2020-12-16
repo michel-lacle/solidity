5fb2bf8d254c0692cabe476f5b5d0906e60af77f
michel <1024836+michel-lacle@users.noreply.github.com>
2020-12-16 18:35:47 -0500

    working deploy of solidit contract

--- compile.js
+++ compile.js
@@ -1,9 +1,14 @@
-const path = require('path');
-const fs = require('fs');
-const solc = require('solc');
+import path from 'path';
+import fs from 'fs';
+import solc from 'solc';
 
 const dirname = path.resolve();
 const inboxPath = path.resolve(dirname, 'contracts', 'Inbox.sol');
 const source = fs.readFileSync(inboxPath, 'utf8');
 
-module.exports = solc.compile(source, 1).contracts[':Inbox'];
+var inbox_contract = solc.compile(source, 1).contracts[':Inbox'];
+
+console.log(inbox_contract);
+
+export let _interface = inbox_contract.interface;
+export let _bytecode = inbox_contract.bytecode
--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -3,15 +3,21 @@ import ganache from 'ganache-cli';
 import Web3 from 'web3';
 
 const web3 = new Web3(ganache.provider());
+import { _interface, _bytecode } from '../compile.js'
 
 let accounts = null;
+let inbox;
 
 beforeEach(async () => {
     accounts = await web3.eth.getAccounts();
+
+    inbox = await new web3.eth.Contract(JSON.parse(_interface))
+        .deploy({ data: _bytecode, arguments: ['Hi There!']})
+        .send({ from: accounts[0], gas: '1000000'});
 });
 
 describe('Inbox', () => {
     it('deploys a contract', () =>{
-        console.log(accounts);
+        console.log(inbox);
     });
 });
